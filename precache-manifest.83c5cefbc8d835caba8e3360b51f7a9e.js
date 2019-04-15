ca0b8e7c21592d5955bf3786a052ba4357f4eb0d
Ciobanu Vlad <ciobanu.vlad@cerner.com>
2019-04-15 18:23:45 +0300

    Updates

---
+++ precache-manifest.83c5cefbc8d835caba8e3360b51f7a9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/weather/static/media/iconfinder_weather01_4102328.3077681e.svg"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "32ad6f1e2a17ea5aecf3",
+    "url": "/weather/static/js/main.dee9de91.chunk.js"
+  },
+  {
+    "revision": "0566ace1edbd85a2242e",
+    "url": "/weather/static/js/2.f15d8982.chunk.js"
+  },
+  {
+    "revision": "32ad6f1e2a17ea5aecf3",
+    "url": "/weather/static/css/main.33cfcd25.chunk.css"
+  },
+  {
+    "revision": "ae119f28691476122820aeb8166bf9eb",
+    "url": "/weather/index.html"
+  }
+];

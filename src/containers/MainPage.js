db39baa66bc1fb18c0211ee6a164294127ccee56
Ciobanu Vlad <ciobanu.vlad@cerner.com>
2019-04-15 23:23:55 +0300

    final

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
-
+// import logo from "./../../public/weather_icons/cloud_sun_1.svg"
+import logo from '../weather_icons/cloud_sun_1.svg';
 class MainPage extends Component {
 
 
     constructor(props, state) {
+        // this.myFunction = this.myFunction.bind(this);
         super(props, state);
+        this.state = {
+            isLoaded: false,
+            items: "TEst"
+        }
     }
+    prepareData(data) {
+        let icon;
+        if (data.currently.icon === "clear-night") {
+            icon = logo;
+        }
+        var newSate = {
+            isLoaded: false,
+            items: data.currently.summary,
+            icon: logo,
+            temperature: data.currently.temperature
+        }
+        return newSate;
+    }
+
     componentDidMount() {
         debugger;
-        const DarkSky = require('dark-sky')
-        const darksky = new DarkSky('a2e9fc452d514f082cc3d7a0adbda5d5') // Your API KEY can be hardcoded, but I recommend setting it as an env variable.
+        const api = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a2e9fc452d514f082cc3d7a0adbda5d5/45.64861,%2025.60613?units=si&lang=ro';
+        fetch(api)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    debugger;
+                    console.table(result)
+                    var newSate = this.prepareData(result);
+                    this.setState(newSate);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    debugger;
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
 
-        darksky
-            .latitude('37.8267')
-            .longitude(-122.423)
-            .get()
-            .then(console.log)
-            .catch(console.log)
     }
 
     render() {
+
         return (
-            <div>TEST</div>
+            <div>
+                <div>{this.state.items}</div>
+                <img src={this.state.icon} alt="logo" />
+                <div>Temperatura : {this.state.temperature}</div>
+            </div>
         )
     }
 }

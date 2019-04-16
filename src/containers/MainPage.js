90b376bea5e6d96b182073564986ffe5e2267073
Ciobanu Vlad <ciobanu.vlad@cerner.com>
2019-04-16 12:43:27 +0300

    change default

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from "./../../public/weather_icons/cloud_sun_1.svg"
 import cloud_sun_1 from '../weather_icons/cloud_sun_1.svg';
 import normal_night from '../weather_icons/normal_nihght.svg';
+import sun from '../weather_icons/sun.svg';
 import heart from '../weather_icons/heart.svg';
 import "@ui5/webcomponents/dist/List";
 import "@ui5/webcomponents/dist/StandardListItem";
@@ -18,19 +19,27 @@ class MainPage extends Component {
         // this.myFunction = this.myFunction.bind(this);
 
         super(props, state);
+        debugger;
+        // var x = navigator.geolocation.getCurrentPosition(this.success);
+
         this.select = React.createRef();
         this.state = {
             isLoaded: false,
             items: "TEst"
         }
+
     }
+
     prepareData(data) {
         var icon;
+        debugger;
         switch (data.currently.icon) {
             case "clear-night":
                 icon = normal_night;
                 break;
-
+            case "clear-day":
+                icon = sun;
+                break;
             default:
                 icon = cloud_sun_1
                 break;
@@ -46,37 +55,47 @@ class MainPage extends Component {
 
 
     changeLocation(event) {
-        let location = event.detail.item.innerHTML
 
-        if (location === "Brasov") {
-            // alert("VLAD" + location);
-            this.setState({
-                isLoaded: false,
-                items: "Foarte cald la noi",
-                icon: heart,
-                temperature: "∞"
-            });
+
+        let location = event.detail.item.innerHTML
+        switch (location) {
+            case "BrasovN":
+                this.setState({
+                    isLoaded: false,
+                    items: "Foarte cald la noi",
+                    icon: heart,
+                    temperature: "∞"
+                });
+                break;
+            case "Berlin":
+                this.getWeatherByLatLong(Constants.BERLIN_LOCATION);
+                break;
+            case "Zarnesti":
+                this.getWeatherByLatLong(Constants.ZARNEST_LOCATION);
+                break;
+            case "Targoviste":
+                this.getWeatherByLatLong(Constants.TARGOVISTE_LOCATION);
+                break;
+            case "Brasov":
+                this.getWeatherByLatLong(Constants.BRASOV_LOCATION);
+                break;
+
+            default:
+                this.setState({
+                    isLoaded: false,
+                    items: "Unknown yet# Under development",
+                    icon: cloud_sun_1,
+                    temperature: ""
+                });
+                break;
         }
-        else {
-            this.setState({
-                isLoaded: false,
-                items: "Unknown yet# Under development",
-                icon: cloud_sun_1,
-                temperature: ""
-            });
-        }
+
+
     }
 
-    componentDidMount() {
+    getWeatherByLatLong(coord) {
 
-        this.select.current.addEventListener('itemPress', event => {
-            this.changeLocation(event);
-
-        });
-
-
-        debugger;
-        const api = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a2e9fc452d514f082cc3d7a0adbda5d5/45.64861,%2025.60613?units=si&lang=ro';
+        const api = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a2e9fc452d514f082cc3d7a0adbda5d5/' + coord + '?units=si&lang=ro';
         fetch(api)
             .then(res => res.json())
             .then(
@@ -99,6 +118,45 @@ class MainPage extends Component {
             )
     }
 
+    componentDidMount() {
+
+        navigator.geolocation.getCurrentPosition(() => {
+            debugger;
+        }
+            , () => {
+                debugger;
+            });
+
+        this.select.current.addEventListener('itemPress', event => {
+            this.changeLocation(event);
+
+        });
+
+        const defaultCoord = "44.92543,25.4567";
+        this.getWeatherByLatLong(defaultCoord);
+        // const api = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a2e9fc452d514f082cc3d7a0adbda5d5/' + defaultCoord + '?units=si&lang=ro';
+        // fetch(api)
+        //     .then(res => res.json())
+        //     .then(
+        //         (result) => {
+        //             debugger;
+        //             console.table(result)
+        //             var newSate = this.prepareData(result);
+        //             this.setState(newSate);
+        //         },
+        //         // Note: it's important to handle errors here
+        //         // instead of a catch() block so that we don't swallow
+        //         // exceptions from actual bugs in components.
+        //         (error) => {
+        //             debugger;
+        //             this.setState({
+        //                 isLoaded: true,
+        //                 error
+        //             });
+        //         }
+        //     )
+    }
+
     render() {
 
         return (
@@ -114,26 +172,29 @@ class MainPage extends Component {
 
                 <div style={{ display: "-webkit-box" }} >
                     <div style={{
-                        width: "40%"
+                        width: "30%",
+                        minWidth: "200px"
                     }}>
                         <ui5-list id="myList" ref={this.select}>
-                            <ui5-li icon="sap-icon://functional-location" type="Active" description="Acasa la ei doi">Brasov</ui5-li>
+                            <ui5-li icon="sap-icon://functional-location" type="Active" description="Acasa la ei doi">BrasovN</ui5-li>
                             <ui5-li icon="sap-icon://functional-location" type="Active" description="Acasa la Cristina">Targoviste</ui5-li>
                             <ui5-li icon="sap-icon://functional-location" type="Active" description="Acasa la Vlad">Zarnesti</ui5-li>
                             <ui5-li icon="sap-icon://functional-location" type="Active" description="Merem la Cluj">Cluj-Napoca</ui5-li>
                             <ui5-li icon="sap-icon://functional-location" type="Active" description="Bucuresti">Bucuresti</ui5-li>
                             <ui5-li icon="sap-icon://functional-location" type="Active" description="Bucuresti">Berlin</ui5-li>
+                            <ui5-li icon="sap-icon://functional-location" type="Active" description="Brasov">Brasov</ui5-li>
 
 
                         </ui5-list>
 
                     </div>
                     <div
-                    // padding: "5% 10px 10px",
-                    // marginTop: "auto",
-                    // marginRight: "auto",
-                    // marginBottom: "auto",
-                    // marginLeft: "20%"
+                        // padding: "5% 10px 10px",
+                        // marginTop: "auto",
+                        // marginRight: "auto",
+                        // marginBottom: "auto",
+                        // marginLeft: "20%"
+                        style={{ marginLeft: "5%", marginTop: "2%" }}
                     >
                         <div>Statusul vremii: {this.state.items}</div>
                         <img src={this.state.icon} alt="logo" />
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,1 +1,8 @@
 export const ICON_PICTURE = "https://scontent.fotp3-1.fna.fbcdn.net/v/t1.0-9/13939519_1218182778232415_7113308434780676075_n.jpg?_nc_cat=110&_nc_ht=scontent.fotp3-1.fna&oh=1c6c88457e6d700a8aadcc83531e3561&oe=5D3A77E0"
+export const BRASOV_LOCATION = "44.92543,25.4567";
+export const TARGOVISTE_LOCATION = "44.92543,25.4567";
+export const BERLIN_LOCATION = "52.520008,13.404954";
+export const CLUJ_LOCATION = "";
+export const BUCURESTI_LOCATION = "";
+export const ZARNEST_LOCATION = "45.515,25.3672"
+
